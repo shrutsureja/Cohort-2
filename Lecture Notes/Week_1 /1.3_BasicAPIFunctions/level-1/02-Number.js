2fda8919f92f373a49e37c534ddcc414276a2ee3
shrutsureja <shrutsureja.code@gmail.com>
2023-12-18 19:38:40 +0530

    numbers completed with notes

--- Lecture Notes/Week_1 /1.3_BasicAPIFunctions/level-1/02-Number.js
+++ Lecture Notes/Week_1 /1.3_BasicAPIFunctions/level-1/02-Number.js
@@ -7,6 +7,8 @@ function explainParseInt(value) {
 // Example Usage for parseInt
 explainParseInt("42");
 explainParseInt("42px");
+explainParseInt("42px24");
+explainParseInt("px24");
 explainParseInt("3.14");
 
 function explainParseFloat(value) {
@@ -18,4 +20,25 @@ function explainParseFloat(value) {
 // Example Usage for parseFloat
 explainParseFloat("3.14");
 explainParseFloat("42");
+explainParseFloat("42.465p");
 explainParseFloat("42px");
+
+let x = 9999999999999998;
+let y = "9999999999999999";
+console.log(x);
+console.log(y.length);
+
+
+let z1 = 10 + 20;// z will number 30
+let z2 = "10" + 20;// z will string 1020
+let z3 = "10" + "20";// z will string   1020  
+let z4 = 10 + "20";// z will string     1020
+let z5 = "the result be-" + 10 + 20;//z will be :the result be-1020
+let z6 = 10 + 20 + "20";
+
+console.log("z1 : " + z1 + "\nType of z1:" + typeof z1);
+console.log("z1 : " + z2 + "\nType of z1:" + typeof z2);
+console.log("z1 : " + z3 + "\nType of z1:" + typeof z3);
+console.log("z1 : " + z4 + "\nType of z1:" + typeof z4);
+console.log("z1 : " + z5 + "\nType of z1:" + typeof z5);
+console.log("z1 : " + z6 + "\nType of z1:" + typeof z6);
